c10846d191b33dd12196e4ee1af30688650ad3cc
Junsu Heo <gjwnstn515@naver.com>
2023-11-12 06:17:01 +0900

    챗봇 메세지 리스트 구현 완료

--- src/components/Chatbot/MessageList.jsx
+++ src/components/Chatbot/MessageList.jsx
@@ -11,15 +11,39 @@ function MessageList({ sends, reply }) {
   //   createElement;
   console.log(sends, reply);
 
+  // 두 배열을 번갈아가며 합치기
+  const combinedArray = [];
+  for (let i = 0; i < Math.max(sends.length, reply.length); i++) {
+    if (i < sends.length) {
+      combinedArray.push({ type: "send", data: sends[i] });
+    }
+    if (i < reply.length) {
+      combinedArray.push({ type: "response", data: reply[i] });
+    }
+  }
+
+  // 합쳐진 배열 렌더링
+  const result = combinedArray.map((item, index) => {
+    if (item.type === "send") {
+      return <PrintSend send={item.data} key={index} />;
+    } else if (item.type === "response") {
+      return <PrintResponse response={item.data} key={index} />;
+    }
+    return null;
+  });
+
   return (
     <>
       <ul className="SendList">
-        {sends.map((element) => (
+        {result}
+
+        {/* {sends.map((element) => (
           <PrintSend send={element} key={element.id} />
         ))}
+
         {reply.map((element) => (
           <PrintResponse response={element} key={element.id2} />
-        ))}
+        ))} */}
       </ul>
 
       {/* <ul className="ResponseList">
